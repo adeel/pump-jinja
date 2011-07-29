f8e7207b58e6c0285731ccd1bbca48c19aa2a328
Adeel Ahmad Khan <adeel@adeel.ru>
2011-07-28 23:21:41 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
   name="pump-jinja",
   version="0.0.1",
   description="A Pump middleware that uses the Jinja templating system to render responses.",
-  long_description=open("README.md").read(),
   author="Adeel Ahmad Khan",
   author_email="adeel@adeel.ru",
   py_modules=["pump_jinja"],
